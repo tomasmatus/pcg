06d91c08b5c8d276daa468d30c245b28a3f20d7b
tomasmatus <tomatus777@gmail.com>
2024-10-22 23:18:16 +0200

    step2 struggles

--- pcg-proj1/Step2/nbody.cu
+++ pcg-proj1/Step2/nbody.cu
@@ -1,10 +1,10 @@
 /**
  * @file      nbody.cu
  *
- * @author    Name Surname \n
+ * @author    Tomáš Matuš \n
  *            Faculty of Information Technology \n
  *            Brno University of Technology \n
- *            xlogin00@fit.vutbr.cz
+ *            xmatus37@fit.vutbr.cz
  *
  * @brief     PCG Assignment 1
  *
@@ -33,8 +33,100 @@ __global__ void calculateVelocity(Particles pIn, Particles pOut, const unsigned 
   /********************************************************************************************************************/
   /*  TODO: CUDA kernel to calculate new particles velocity and position, use shared memory to minimize memory access */
   /********************************************************************************************************************/
+  extern __shared__ float sharedMem[];
 
-  
+  const unsigned threadID = threadIdx.x + blockIdx.x * blockDim.x;
+  if (threadID >= N) {
+    return;
+  }
+
+  float* const pPosX = pIn.posX;
+  float* const pPosY = pIn.posY;
+  float* const pPosZ = pIn.posZ;
+  float* const pVelX = pIn.velX;
+  float* const pVelY = pIn.velY;
+  float* const pVelZ = pIn.velZ;
+  float* const pweight = pIn.weight;
+
+  float* const pOutPosX = pOut.posX;
+  float* const pOutPosY = pOut.posY;
+  float* const pOutPosZ = pOut.posZ;
+  float* const pOutVelX = pOut.velX;
+  float* const pOutVelY = pOut.velY;
+  float* const pOutVelZ = pOut.velZ;
+
+  // tmp velocity
+  float newGravitationVelX{};
+  float newGravitationVelY{};
+  float newGravitationVelZ{};
+
+  float newCollisionVelX{};
+  float newCollisionVelY{};
+  float newCollisionVelZ{};
+
+  // current particle
+  const float posX = pPosX[threadID];
+  const float posY = pPosY[threadID];
+  const float posZ = pPosZ[threadID];
+  const float velX = pVelX[threadID];
+  const float velY = pVelY[threadID];
+  const float velZ = pVelZ[threadID];
+  const float weight = pweight[threadID];
+
+  for (unsigned j = 0u; j < N; j++) {
+    // neighbour partcle
+    const float otherPosX = pPosX[j];
+    const float otherPosY = pPosY[j];
+    const float otherPosZ = pPosZ[j];
+    const float otherVelX = pVelX[j];
+    const float otherVelY = pVelY[j];
+    const float otherVelZ = pVelZ[j];
+    const float otherWeight = pweight[j];
+
+    // distance between particles in dimensions
+    const float dx = otherPosX - posX;
+    const float dy = otherPosY - posY;
+    const float dz = otherPosZ - posZ;
+
+    // distance r between particles in 3D
+    const float r2 = dx * dx + dy * dy + dz * dz;
+    const float r = std::sqrt(r2) + __FLT_MIN__;
+
+    // gravity force of the two particles
+    const float f = G * weight * otherWeight / r2 + __FLT_MIN__;
+
+    // SUM(F^(i+1))
+    newGravitationVelX += (r > COLLISION_DISTANCE) ? dx / r * f : 0.f;
+    newGravitationVelY += (r > COLLISION_DISTANCE) ? dy / r * f : 0.f;
+    newGravitationVelZ += (r > COLLISION_DISTANCE) ? dz / r * f : 0.f;
+
+    newCollisionVelX += (r > 0.f && r < COLLISION_DISTANCE)
+                ? (((weight * velX - otherWeight * velX + 2.f * otherWeight * otherVelX) / (weight + otherWeight)) - velX)
+                : 0.f;
+    newCollisionVelY += (r > 0.f && r < COLLISION_DISTANCE)
+                ? (((weight * velY - otherWeight * velY + 2.f * otherWeight * otherVelY) / (weight + otherWeight)) - velY)
+                : 0.f;
+    newCollisionVelZ += (r > 0.f && r < COLLISION_DISTANCE)
+                ? (((weight * velZ - otherWeight * velZ + 2.f * otherWeight * otherVelZ) / (weight + otherWeight)) - velZ)
+                : 0.f;
+  }
+
+  // Final results from the first kernel in step0
+  newGravitationVelX *= dt / weight;
+  newGravitationVelY *= dt / weight;
+  newGravitationVelZ *= dt / weight;
+
+  const float nextStepVelX = velX + newGravitationVelX + newCollisionVelX;
+  const float nextStepVelY = velY + newGravitationVelY + newCollisionVelY;
+  const float nextStepVelZ = velZ + newGravitationVelZ + newCollisionVelZ;
+
+  pOutPosX[threadID] = posX + nextStepVelX * dt;
+  pOutPosY[threadID] = posY + nextStepVelY * dt;
+  pOutPosZ[threadID] = posZ + nextStepVelZ * dt;
+
+  pOutVelX[threadID] = nextStepVelX;
+  pOutVelY[threadID] = nextStepVelY;
+  pOutVelZ[threadID] = nextStepVelZ;
 }// end of calculate_gravitation_velocity
 //----------------------------------------------------------------------------------------------------------------------
 
